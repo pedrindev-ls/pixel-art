a7273434714cd6971ebdecb57e7080a71eb160e1
Pedrin <pedro515vieira@gmail.com>
2022-01-20 10:39:14 -0300

    lint

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-let cor1 = document.querySelector('.black')
-let cor2 = document.querySelector('.red')
-let cor3 = document.querySelector('.blue')
-let cor4 = document.querySelector('.green')
+let cor1 = document.querySelector('.black');
+let cor2 = document.querySelector('.red');
+let cor3 = document.querySelector('.blue');
+let cor4 = document.querySelector('.green');
 
-function className (event){
-    let lugar = document.querySelector('.selected')
-    lugar.classList.remove('selected')
-    event.target.classList.add('selected')
+function className(event) {
+  const lugar = document.querySelector('.selected');
+  lugar.classList.remove('selected');
+  event.target.classList.add('selected');
 }
 
-cor1.addEventListener('click', className)
-cor2.addEventListener('click', className)
-cor3.addEventListener('click', className)
-cor4.addEventListener('click', className)
+cor1.addEventListener('click', className);
+cor2.addEventListener('click', className);
+cor3.addEventListener('click', className);
+cor4.addEventListener('click', className);
 
-let quad1 = document.querySelector('#quad1')
-let quad2 = document.querySelector('#quad2')
-let quad3 = document.querySelector('#quad3')
-let quad4 = document.querySelector('#quad4')
-let quad5 = document.querySelector('#quad5')
-let quad6 = document.querySelector('#quad6')
-let quad7 = document.querySelector('#quad7')
-let quad8 = document.querySelector('#quad8')
-let quad9 = document.querySelector('#quad9')
-let quad10 = document.querySelector('#quad10')
-let quad11 = document.querySelector('#quad11')
-let quad12 = document.querySelector('#quad12')
-let quad13 = document.querySelector('#quad13')
-let quad14 = document.querySelector('#quad14')
-let quad15 = document.querySelector('#quad15')
-let quad16 = document.querySelector('#quad16')
-let quad17 = document.querySelector('#quad17')
-let quad18 = document.querySelector('#quad18')
-let quad19 = document.querySelector('#quad19')
-let quad20 = document.querySelector('#quad20')
-let quad21 = document.querySelector('#quad21')
-let quad22 = document.querySelector('#quad22')
-let quad23 = document.querySelector('#quad23')
-let quad24 = document.querySelector('#quad24')
-let quad25 = document.querySelector('#quad25')
+const quad1 = document.querySelector('#quad1');
+const quad2 = document.querySelector('#quad2');
+const quad3 = document.querySelector('#quad3');
+const quad4 = document.querySelector('#quad4');
+const quad5 = document.querySelector('#quad5');
+const quad6 = document.querySelector('#quad6');
+const quad7 = document.querySelector('#quad7');
+const quad8 = document.querySelector('#quad8');
+const quad9 = document.querySelector('#quad9');
+const quad10 = document.querySelector('#quad10');
+const quad11 = document.querySelector('#quad11');
+const quad12 = document.querySelector('#quad12');
+const quad13 = document.querySelector('#quad13');
+const quad14 = document.querySelector('#quad14');
+const quad15 = document.querySelector('#quad15');
+const quad16 = document.querySelector('#quad16');
+const quad17 = document.querySelector('#quad17');
+const quad18 = document.querySelector('#quad18');
+const quad19 = document.querySelector('#quad19');
+const quad20 = document.querySelector('#quad20');
+const quad21 = document.querySelector('#quad21');
+const quad22 = document.querySelector('#quad22');
+const quad23 = document.querySelector('#quad23');
+const quad24 = document.querySelector('#quad24');
+const quad25 = document.querySelector('#quad25');
 
 
-function paint (event){
+function paint(event) {
     let lugar = document.querySelector('.selected')
     if (lugar === cor1){
         event.target.classList.add('black')
@@ -66,39 +66,39 @@ function paint (event){
     }
 }
 
-quad1.addEventListener('click', paint)
-quad2.addEventListener('click', paint)
-quad3.addEventListener('click', paint)
-quad4.addEventListener('click', paint)
-quad5.addEventListener('click', paint)
-quad6.addEventListener('click', paint)
-quad7.addEventListener('click', paint)
-quad8.addEventListener('click', paint)
-quad9.addEventListener('click', paint)
-quad10.addEventListener('click', paint)
-quad11.addEventListener('click', paint)
-quad12.addEventListener('click', paint)
-quad13.addEventListener('click', paint)
-quad14.addEventListener('click', paint)
-quad15.addEventListener('click', paint)
-quad16.addEventListener('click', paint)
-quad17.addEventListener('click', paint)
-quad18.addEventListener('click', paint)
-quad19.addEventListener('click', paint)
-quad20.addEventListener('click', paint)
-quad21.addEventListener('click', paint)
-quad22.addEventListener('click', paint)
-quad23.addEventListener('click', paint)
-quad24.addEventListener('click', paint)
-quad25.addEventListener('click', paint)
+quad1.addEventListener('click', paint);
+quad2.addEventListener('click', paint);
+quad3.addEventListener('click', paint);
+quad4.addEventListener('click', paint);
+quad5.addEventListener('click', paint);
+quad6.addEventListener('click', paint);
+quad7.addEventListener('click', paint);
+quad8.addEventListener('click', paint);
+quad9.addEventListener('click', paint);
+quad10.addEventListener('click', paint);
+quad11.addEventListener('click', paint);
+quad12.addEventListener('click', paint);
+quad13.addEventListener('click', paint);
+quad14.addEventListener('click', paint);
+quad15.addEventListener('click', paint);
+quad16.addEventListener('click', paint);
+quad17.addEventListener('click', paint);
+quad18.addEventListener('click', paint);
+quad19.addEventListener('click', paint);
+quad20.addEventListener('click', paint);
+quad21.addEventListener('click', paint);
+quad22.addEventListener('click', paint);
+quad23.addEventListener('click', paint);
+quad24.addEventListener('click', paint);
+quad25.addEventListener('click', paint);
 
-let reset = document.querySelector('#clear-board')
-reset.addEventListener('click', function(){
-    let pixels = document.getElementsByClassName('pixel')
-    for (let i of pixels){
-        i.classList.remove('black')
-        i.classList.remove('red')
-        i.classList.remove('blue')
-        i.classList.remove('green')
+const reset = document.querySelector('#clear-board');
+reset.addEventListener('click', function() {
+    let pixels = document.getElementsByClassName('pixel');
+    for (let i of pixels) {
+        i.classList.remove('black');
+        i.classList.remove('red');
+        i.classList.remove('blue');
+        i.classList.remove('green');
     }
 })
